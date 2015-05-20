785dbb0435a39a34864ddad06e2c60f48cc680a6
Dorian Kusznir <doriank87@gmail.com>
2015-05-20 09:53:10 -0400

    added save button

--- blocnotes/blocnotes/DetailViewController.m
+++ blocnotes/blocnotes/DetailViewController.m
@@ -14,6 +14,7 @@
 @property (nonatomic, strong) UIBarButtonItem *saveButton;
 @property (weak, nonatomic) UILabel *detailDescriptionLabel;
 @property (nonatomic, strong) UILabel *createdDateAndTime;
+@property (nonatomic, strong) UILabel *savedLabel;
 
 @end
 
@@ -67,8 +68,19 @@
     self.createdDateAndTime.backgroundColor = [UIColor colorWithRed:0 green:0 blue:153.0 alpha:1];
     [self.createdDateAndTime setFont:[UIFont fontWithName:@"HelveticaNeue" size:15]];
     
+    self.savedLabel = [[UILabel alloc] initWithFrame:CGRectMake(0, 0, (self.view.bounds.size.width / 2), 40)];
+    self.savedLabel.backgroundColor = [UIColor colorWithRed:0.0 green:245.0 blue:0.0 alpha:1.0];
+    self.savedLabel.layer.cornerRadius = 5.0;
+    self.savedLabel.clipsToBounds = YES;
+    self.savedLabel.text = [NSString stringWithFormat:NSLocalizedString(@"Saved!", @"Saved Label")];
+    self.savedLabel.textColor = [UIColor colorWithRed:12.0 green:12.0 blue:12.0 alpha:1.0];
+    self.savedLabel.textAlignment = NSTextAlignmentCenter;
+    self.savedLabel.alpha = 0;
+    self.savedLabel.center = self.textView.center;
+    
     [self.view addSubview:self.textView];
     [self.view addSubview:self.createdDateAndTime];
+    [self.view addSubview:self.savedLabel];
     self.navigationItem.rightBarButtonItem = self.saveButton;
     
 }
@@ -104,6 +116,18 @@
     //[self.detailItem setValue:self.text forKey:@"content"];
     NSLog(@"%@", self.text);
     [self.delegate didUpdate:self withText:self.text];
+    [self displaySavedButton];
+}
+
+- (void)displaySavedButton
+{
+    [UIView animateWithDuration:2.0 delay:0 options:UIViewAnimationOptionCurveEaseIn animations:^{
+        self.savedLabel.alpha = 0.75;
+    } completion:nil];
+    
+    [UIView animateWithDuration:2.0 delay:2.0 options:UIViewAnimationOptionCurveEaseOut animations:^{
+        self.savedLabel.alpha = 0;
+    } completion:nil];
 }
 
 @end
