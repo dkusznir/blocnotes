528008d99a3386cb5640a0b9c5ee994c3e203639
Dorian Kusznir <doriank87@gmail.com>
2015-05-26 18:27:43 -0400

    note

---
+++ blocnotes/Note.h
@@ -0,0 +1,13 @@
+//
+//  Note.h
+//  blocnotes
+//
+//  Created by Dorian Kusznir on 5/24/15.
+//  Copyright (c) 2015 dkusznir. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Note : NSObject
+
+@end
---
+++ blocnotes/Note.m
@@ -0,0 +1,13 @@
+//
+//  Note.m
+//  blocnotes
+//
+//  Created by Dorian Kusznir on 5/24/15.
+//  Copyright (c) 2015 dkusznir. All rights reserved.
+//
+
+#import "Note.h"
+
+@implementation Note
+
+@end
